8d3cb4e203922af1f68138c478180fb1733dac97
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2025-05-06 09:47:22 -0700

    deployed backend successfully

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,24 +1,29 @@
-require('dotenv').config(); // Add this at the top
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres"
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false // for Render PostgreSQL
+        rejectUnauthorized: false,
       }
     }
   }
